a58e264792d679c8d36567f1d0305c61c3f90de4
melissa-murphy <melissamurphy.ca@gmail.com>
2019-04-04 13:34:24 -0700

    created classes, starting new objects

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,64 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(personAttr) {
+        this.name = personAttr.name;
+        this.age = personAttr.age;
+        this.location = personAttr.location;
+        this.gender = personAttr.gender;
+    }//Methods
+    speak() {
+        return `Hello, my name is ${name}, and I am from ${location}.`;
+    }
+}//Person
+
+class Instructor extends Person {
+    constructor(instructorAttr) {
+        super(instructorAttr);
+        this.speciality = instructorAttr.speciality;
+        this.favLanguage = instructorAttr.favLanguage;
+        this.catchPhrase = instructorAttr.catchPhrase;
+    }// Methods
+    demo() {
+        //rcv string and log out 'Today we are learning about {subject}' 
+    }
+    grade() {
+        //rcv student object, subject string, and logs '{student.name} receives a perfect score on {subject}'
+    }
+}// Instructor
+
+class Student extends Person {
+    constructor(studentAttr) {
+        super(studentAttr);
+        this.previousBg = studentAttr.previousBg;
+        this.className = studentAttr.className;
+        this.favSubjects = studentAttr.favSubjects
+    }// Methods
+    listsSubjects() {
+        // console favSubjects
+    }
+    PRAssignment() {
+        // rcv (subject) and log student.name has submitted a PR for {subject}
+    }
+    sprintChallenge() {
+        // rcv (subject) and log student.name has begun sprint challenge on {subject}
+    }
+}// Student
+
+class ProjectManagers extends Instructor {
+    constructor(pmAttr) {
+        super(pmAttr);
+        this.gradClassName = pmAttr.gradClassName;
+        this.favInstructor = pmAttr.favInstructor;
+    }// Methods
+    standUp() {
+        // take slack channel and log `{name} announces to {channel}, @channel standy times!​​​​​
+    }
+    deBugsCode() {
+        // takes in student object and log {name} debugs {student.name}'s code on {subject}
+    }
+}// Project Managers
+
+//Instructor templates
+const bob = new Instructor({
+
+})
